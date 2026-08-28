51faffd1215ee616e430bb54c2ff5561c5291f1f
Allan <maquetofoda@gmail.com>
2026-08-28 08:38:09 -0300

    Implement admin logout handler

--- api/admin-logout.js
+++ api/admin-logout.js
@@ -1,1 +1,9 @@
+import { clearAdminCookie } from './_auth.js';
 
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método não permitido' });
+  }
+  res.setHeader('Set-Cookie', clearAdminCookie());
+  return res.status(200).json({ ok: true });
+}
